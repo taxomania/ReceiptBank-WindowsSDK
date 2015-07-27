7ae3b0ce2ebe3640ea88259e9273e19e043caa38
Tariq Patel <tariq.patel@outlook.com>
2015-07-28 00:14:55 +0100

    package info

--- ReceiptBankLibrary/Properties/AssemblyInfo.cs
+++ ReceiptBankLibrary/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Receipt Bank Windows SDK")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Receipt Bank Windows 10 SDK")]
+[assembly: AssemblyDescription("Simple Windows 10 SDK for Receipt Bank API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Taxomania")]
 [assembly: AssemblyProduct("ReceiptBankSDK")]
